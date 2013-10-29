3bec902d58a367874113551d94dfa60ff66edc68
Trevor Cooper <tcooper@sdsc.edu>
2013-10-29 10:47:20 -0700

    Modified Makefile and version.mk for mpfr to work with pull.mk

--- src/mpfr/version.mk
+++ src/mpfr/version.mk
@@ -1,3 +1,11 @@
 NAME    = opt-mpfr
 VERSION = 3.1.2
 RELEASE = 0
+
+SRC_SUBDIR	= mpfr
+
+MPFR_NAME	= mpfr
+MPFR_VERSION	= 3.1.2
+
+# List of *.tar.gz packages that are part of THIS package
+TAR_GZ_PKGS = $(MPFR_NAME)-$(MPFR_VERSION).tar.gz
