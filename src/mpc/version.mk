a18338edeeead673921122100529a2ff9c72131c
Trevor Cooper <tcooper@sdsc.edu>
2013-10-29 10:46:50 -0700

    Modified Makefile and version.mk for mpc to work with pull.mk

--- src/mpc/version.mk
+++ src/mpc/version.mk
@@ -1,3 +1,11 @@
 NAME    = opt-mpc
 VERSION = 1.0.1
 RELEASE = 0
+
+SRC_SUBDIR	= mpc
+
+MPC_NAME	= mpc
+MPC_VERSION	= 1.0.1
+
+# List of *.tar.gz packages that are part of THIS package
+TAR_GZ_PKGS = $(MPC_NAME)-$(MPC_VERSION).tar.gz
