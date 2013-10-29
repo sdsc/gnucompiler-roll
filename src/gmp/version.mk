6e1f52186067c2386da868ef857e85a557f49dbc
Trevor Cooper <tcooper@sdsc.edu>
2013-10-29 10:46:22 -0700

    Modified Makefile and version.mk for gmp to work with pull.mk

--- src/gmp/version.mk
+++ src/gmp/version.mk
@@ -1,3 +1,12 @@
 NAME    = opt-gmp
 VERSION = 5.1.2
 RELEASE = 0
+
+SRC_SUBDIR	= gmp
+
+GMP_NAME	= gmp
+GMP_VERSION	= 5.1.2
+
+# List of *.tar.gz packages that are part of THIS package
+TAR_GZ_PKGS = $(GMP_NAME)-$(GMP_VERSION).tar.gz
+
