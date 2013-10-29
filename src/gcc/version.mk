a47cffe3eec20bbbce7525d2d1df18002edfbb73
Trevor Cooper <tcooper@sdsc.edu>
2013-10-29 10:45:01 -0700

    Modified Makefile and version.mk for gcc to work with pull.mk

--- src/gcc/version.mk
+++ src/gcc/version.mk
@@ -1,3 +1,12 @@
 NAME    = opt-gcc
 VERSION = 4.8.1
 RELEASE = 0
+
+SRC_SUBDIR	= gcc
+
+GCC_NAME	= gcc
+GCC_VERSION	= 4.8.1
+
+# List of *.tar.gz packages that are part of THIS package
+TAR_GZ_PKGS = $(GCC_NAME)-$(GCC_VERSION).tar.gz
+
