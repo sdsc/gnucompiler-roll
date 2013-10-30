197374d694171467c4cd0ed9f9291176c52a4347
Jerry Greenberg <jpg@sdsc.edu>
2013-10-29 17:22:51 -0700

    new gcc version: 4.8.2

--- src/gcc/version.mk
+++ src/gcc/version.mk
@@ -1,11 +1,11 @@
 NAME    = opt-gcc
-VERSION = 4.8.1
+VERSION = 4.8.2
 RELEASE = 0
 
 SRC_SUBDIR	= gcc
 
 GCC_NAME	= gcc
-GCC_VERSION	= 4.8.1
+GCC_VERSION	= 4.8.2
 
 # List of *.tar.gz packages that are part of THIS package
 TAR_GZ_PKGS = $(GCC_NAME)-$(GCC_VERSION).tar.gz
