b92661ec526d7f108ef733eda9f8adbacd04d688
Jim Hayes <jhayes@sdsc.edu>
2014-02-26 16:33:14 -0800

    Update to v1.0.2.

--- src/mpc/version.mk
+++ src/mpc/version.mk
@@ -1,12 +1,11 @@
 NAME    = opt-mpc
-VERSION = 1.0.1
+VERSION = 1.0.2
 RELEASE = 0
 RPM.EXTRAS         = AutoReq:No
 
 SRC_SUBDIR	= mpc
 
 MPC_NAME	= mpc
-MPC_VERSION	= 1.0.1
 
 # List of *.tar.gz packages that are part of THIS package
-TAR_GZ_PKGS = $(MPC_NAME)-$(MPC_VERSION).tar.gz
+TAR_GZ_PKGS = $(MPC_NAME)-$(VERSION).tar.gz
