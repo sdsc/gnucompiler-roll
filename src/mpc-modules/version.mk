10a5f067603bca9eaaddf943406edc626cbd535b
Trevor Cooper <tcooper@sdsc.edu>
2017-09-09 18:00:30 -0700

    Add RPM.PREFIX option to version.mk

--- src/mpc-modules/version.mk
+++ src/mpc-modules/version.mk
@@ -2,7 +2,7 @@ PACKAGE     = mpc
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 1
+RELEASE     = 2
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
@@ -10,3 +10,4 @@ VERSION_INC = version.inc
 include $(VERSION_INC)
 
 RPM.EXTRAS  = AutoReq:No
+RPM.PREFIX  = $(PKGROOT)
--- src/mpfr-modules/version.mk
+++ src/mpfr-modules/version.mk
@@ -2,7 +2,7 @@ PACKAGE     = mpfr
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 2
+RELEASE     = 3
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
@@ -10,3 +10,4 @@ VERSION_INC = version.inc
 include $(VERSION_INC)
 
 RPM.EXTRAS  = AutoReq:No
+RPM.PREFIX  = $(PKGROOT)
